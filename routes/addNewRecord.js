a69c200d665d14fd40bea8eba477efff5585a06f
MARK <marktsai197766@yahoo.com.tw>
2022-03-29 21:59:05 +0800

    0329night

--- routes/addNewRecord.js
+++ routes/addNewRecord.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../services/db');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
 
 router.get('/', async function(req, res, next) {
   try {
     res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    const member_name = req.body.member_name;
+    const lesson_name = req.body.lesson_name;
+    const class_date = req.body.class_date;
+
+    app.use(cors());
+    app.use(bodyParser.urlencoded({extended:true}));
+
     const rows = await db.query(
-      `SELECT member_name FROM members `
+      `INSERT INTO classRecord (member_name, lesson_name, class_date) VALUE (?,?,?)`,
+      [member_name, lesson_name, class_date], (err, result) => {
+        console.log(result);
+      }
     );
     res.json(rows);
   } catch (err) {
